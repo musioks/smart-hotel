faa2c683b565caaab64c90e6219bfa21ebf9bca8
David Musyoka <chandydavid88@gmail.com>
2018-03-29 13:57:55 +0300

    did some little clean up

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -30,7 +30,7 @@
 @else
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Smart-Joint Hotel</a>
+  <a class="navbar-brand" href="#">TOSHA HOTEL MANAGEMENT SYSTEM</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="  navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
